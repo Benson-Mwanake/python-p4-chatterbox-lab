55922368a9176d217b63bd40c809d8c9298c3fa9
Benson <bensonmwanake79@gmail.com>
2025-10-02 19:03:18 +0300

    finished lab

--- server/models.py
+++ server/models.py
@@ -1,14 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+    # Prevent circular recursion when serializing
+    serialize_rules = ()
+
+    def __repr__(self):
+        return f"<Message {self.id}, {self.username}, {self.body}>"
